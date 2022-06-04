d816abcab40a96eafc884fe1efdc8763ac24f1d1
tallesChristian <72113346+tallesChristian@users.noreply.github.com>
2022-06-04 09:09:20 -0300

    Adicionar arquivos de projeto.

---
+++ TelaLogin/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace TelaLogin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.butaoEnviar = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nome = new System.Windows.Forms.TextBox();
+            this.email = new System.Windows.Forms.TextBox();
+            this.senha = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // butaoEnviar
+            // 
+            this.butaoEnviar.Location = new System.Drawing.Point(168, 207);
+            this.butaoEnviar.Name = "butaoEnviar";
+            this.butaoEnviar.Size = new System.Drawing.Size(95, 25);
+            this.butaoEnviar.TabIndex = 0;
+            this.butaoEnviar.Text = "Enviar";
+            this.butaoEnviar.UseVisualStyleBackColor = true;
+            this.butaoEnviar.Click += new System.EventHandler(this.butaoEnviar_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(144, 170);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(178, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Receber notificações por email?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(54, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Senha";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(54, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nome";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // nome
+            // 
+            this.nome.Location = new System.Drawing.Point(108, 59);
+            this.nome.Name = "nome";
+            this.nome.Size = new System.Drawing.Size(286, 20);
+            this.nome.TabIndex = 4;
+            this.nome.Text = "Digite seu nome completo...";
+            this.nome.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // email
+            // 
+            this.email.Location = new System.Drawing.Point(108, 98);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(286, 20);
+            this.email.TabIndex = 5;
+            this.email.Text = "exemplo@email.com";
+            this.email.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // senha
+            // 
+            this.senha.Location = new System.Drawing.Point(108, 133);
+            this.senha.Name = "senha";
+            this.senha.Size = new System.Drawing.Size(286, 20);
+            this.senha.TabIndex = 6;
+            this.senha.Text = "**********";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(57, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Email";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 244);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.senha);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.nome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.butaoEnviar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button butaoEnviar;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox nome;
+        private System.Windows.Forms.TextBox email;
+        private System.Windows.Forms.TextBox senha;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ TelaLogin/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TelaLogin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void butaoEnviar_Click(object sender, EventArgs e)
+        {
+            var nm = nome;
+            var eml = email;
+            var snh = senha;
+
+            MessageBox.Show("Login efetuado com sucesso!!");
+
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
